af2c356ac28f9a45e2c69bfa50c39dc8f5ccd58e
Patrickkibe <patrickdeveloper22@gmail.com>
2023-02-01 18:54:44 +0300

    Login and SignUp for inserted

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,30 +1,69 @@
 import React, { Component } from "react";
-import { Button, Form, FloatingLabel } from "react-bootstrap";
+
+import { Link } from "react-router-dom";
+
+import "../App.css";
+
 export default class Login extends Component {
   render() {
     return (
-        
-      <div>
-        <FloatingLabel controlId="floatingInput">
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="Email Adreess e.g JohnDoe@gmail.com"
-              />
-            </Form.Group>
+      <div className="text-center m-5-auto">
+        <h2 class="title">LOGIN</h2>
+        <h5>Login To Account</h5>
+        <form action="/home">
+          <p>
+            <label for="Username">Username</label>
+            <br />
+            <input
+              type="text"
+              id="username"
+              class="input"
+              autocomplete="off"
+              placeholder="Username..."
+              name="first_name"
+              required
+            />
+          </p>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password..." />
-            </Form.Group>
+          <p>
+            <label for="emailaddress">Email-Address</label>
+            <br />
+            <input
+              type="text"
+              id="emailaddress"
+              class="input"
+              autocomplete="off"
+              placeholder="Email Address..."
+            />
+          </p>
 
-            <Button variant="danger" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </FloatingLabel>
+          <p>
+            <label for="password">Password</label>
+            <br />
+            <input
+              type="text"
+              id="password"
+              class="input"
+              autocomplete="off"
+              placeholder="Password..."
+            />
+          </p>
+
+          <p>
+            <button id="sub_btn" type="submit">
+              LOGIN
+            </button>
+          </p>
+        </form>
+
+        <footer>
+          <p>
+            First time? <Link to="/signup">Create an account</Link>
+          </p>
+          <p>
+            <Link to="/">Back to Homepage</Link>
+          </p>
+        </footer>
       </div>
     );
   }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,13 +1,68 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class SignUp extends Component {
-    render() {
-        return (
-            <div>
-                SIGN-UP PAGE FORM
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="text-center m-5-auto">
+        <h2>SignUp Today...</h2>
+        <h5>Create your personal account</h5>
+
+        <form action="/home">
+          <p>
+            <label>Username</label>
+            <br />
+            <input
+              type="text"
+              id="username"
+              class="input"
+              autocomplete="off"
+              placeholder="Username..."
+              required
+            />
+          </p>
+
+          <p>
+            <label>Email address</label>
+            <br />
+            <input
+              type="email"
+              id="email"
+              class="input"
+              autocomplete="off"
+              placeholder="EmailAddress..."
+              required
+            />
+          </p>
+
+          <p>
+            <label>Password</label>
+            <br />
+            <input
+              type="text"
+              id="password"
+              class="input"
+              autocomplete="off"
+              placeholder="Password..."
+              required
+            />
+          </p>
+
+          <p>
+            <button id="sub_btn" type="submit">
+              SignUp
+            </button>
+          </p>
+        </form>
+
+        <footer>
+          <p>
+            <Link to="/">Back to Homepage</Link>
+          </p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
